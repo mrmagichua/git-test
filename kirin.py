fa6999d39df9aaf41ee411aa56947e45c6fce088
Kirin <kirin.m.hua@gmail.com>
2022-03-01 13:58:28 +0800

    First Commit

---
+++ kirin.py
@@ -0,0 +1,35 @@
+import flask
+
+app = flask.Flask(__name__) #初始化app对象，当前模块名称name
+
+@app.route("/hello")#告诉服务器你的url地址
+def hello():
+    return "hello flask"
+
+
+
+@app.route("/get_grades")
+def get_grades():
+    grades = []
+    with open("input.txt") as fin:
+        for line in fin:
+            line = line.strip()
+            fields = line.split("\t")
+            grades.append(fields)
+    import json
+    return json.dumps(grades) #返回json的字符串
+
+@app.route("/get_grades_byid/<sid>")#设置id查找成绩
+def get_grades_byid(sid):#参数给到func
+    grades = []
+    with open("input.txt") as fin:
+        for line in fin:
+            line = line.strip()
+            fields = line.split("\t")
+            if str(sid) == fields[0]:
+                grades.append(fields)
+
+    import json
+    return json.dumps(grades) #返回json的字符串
+
+app.run()
